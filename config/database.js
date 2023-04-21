505505d81ba8fbc192a3e2735447f0e0ec5c2ab1
vighnesh <vighnesh2607@gmail.com>
2023-04-21 18:30:19 +0530

    second commit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB= async ()=>{
 
-    const {connection}=await mongoose.connect(process.env.MONGO_URI);
+    const {connection}=await mongoose.connect("mongodb+srv://vighneshhegde:passwordforviggy@cluster0.mbil8ch.mongodb.net/coursebundler?retryWrites=true");
 
     console.log(`MongoDB is connected on ${connection.host}`);
 }
