e59a9097b3c5084879956ee10a56110b24aab2ca
vighnesh <vighnesh2607@gmail.com>
2023-04-26 18:22:59 +0530

    fifth commit

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import payment from "./routes/paymentRoutes.js";
 import  ErrorMiddleware  from "./middlewares/Error.js";
 import cookieParser from "cookie-parser";
 import other from "./routes/otherRoutes.js";
+import cors from "cors";
 
 config({
     path:'./config/config.env',
